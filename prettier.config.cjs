6de2489d709536da5ae18d6fc233ce6eed943225
Trent Howard <trent.howard.42@gmail.com>
2023-08-11 10:26:17 +1000

    initial commit

---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+/* eslint-env node */
+/** @type {import("prettier").Options} */
+const config = {
+  trailingComma: 'es5',
+  singleQuote: true,
+};
+
+module.exports = config;
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>Quiz</div>;
+}
+
+export default App;
