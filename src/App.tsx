5b83ec09c92b8b0753d5664983c8a320ee5263e4
Trent Howard <trent.howard.42@gmail.com>
2023-08-14 11:46:42 +1000

    add components and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,86 @@
+import { useState } from 'react';
+
+import { fetchQuiz, type Question } from './api';
+import QuestionCard from './components/QuestionCard';
+
 import './index.css';
 
+export type UserAnswer = {
+  answer: string;
+  correctAnswer: string;
+  isCorrect: boolean;
+};
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Quiz</h1>;
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [current, setCurrent] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<UserAnswer[]>([]);
+
+  const startQuiz = async () => {
+    setLoading(true);
+    setUserAnswers([]);
+
+    const questions = await fetchQuiz();
+
+    setQuestions(questions);
+    setLoading(false);
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const answer = e.currentTarget.value;
+    const correctAnswer = questions[current].correct_answer;
+    const isCorrect = correctAnswer === answer;
+
+    setUserAnswers((a) => [...a, { answer, correctAnswer, isCorrect }]);
+  };
+
+  const nextQuestion = () =>
+    setCurrent((c) => Math.min(c + 1, questions.length - 1));
+
+  const gameOver =
+    userAnswers.length === questions.length && Boolean(questions.length);
+  const score = userAnswers.filter((ans) => ans.isCorrect).length;
+
+  return (
+    <div className="flex h-screen w-full items-center justify-center p-4 align-middle">
+      <div className="flex flex-col items-center gap-6 rounded-xl border-4 border-indigo-600 p-10">
+        <h1 className="text-6xl font-bold">React Quiz!</h1>
+        {(gameOver || !questions.length) && (
+          <div>
+            <button className="btn" onClick={startQuiz}>
+              Start
+            </button>
+          </div>
+        )}
+        {loading && <p>Loading question...</p>}
+        {!gameOver && !loading && (
+          <p className="text-4xl font-bold">Score: {score}</p>
+        )}
+        {Boolean(questions.length) && (
+          <>
+            <QuestionCard
+              questionNb={current + 1}
+              totalQuestions={questions.length}
+              question={questions[current].question}
+              answers={questions[current].answers}
+              userAnswer={userAnswers[current] || ''}
+              callback={checkAnswer}
+            />
+            <div>
+              <button
+                className="btn"
+                onClick={nextQuestion}
+                disabled={gameOver || !userAnswers[current]}
+              >
+                Next question
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,49 @@
+import { type UserAnswer } from '../App';
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: React.MouseEventHandler<HTMLButtonElement>;
+  userAnswer: UserAnswer;
+  questionNb: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNb,
+  totalQuestions,
+}) => {
+  return (
+    <div className="flex max-w-xl flex-col items-center gap-6 text-xl">
+      <p>
+        Question: {questionNb} / {totalQuestions}
+      </p>
+      <p className="text-center">{question}</p>
+      <div className="flex flex-row justify-center gap-3">
+        {answers.map((answer) => (
+          <button
+            className={`btn ${
+              userAnswer.correctAnswer === answer ? 'btn-green' : ''
+            } ${
+              userAnswer.answer === answer && !userAnswer.isCorrect
+                ? 'btn-red'
+                : ''
+            }`}
+            key={answer}
+            value={answer}
+            disabled={Boolean(userAnswer)}
+            onClick={callback}
+          >
+            {answer}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
